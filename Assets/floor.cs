a6e3a402cfb20e6bad4b5374066d0c60dacdfda6
sacemakesgame <mungkinchristian@apps.ipb.ac.id>
2025-06-26 14:49:49 +0700

    idk

--- Assets/floor.cs
+++ Assets/floor.cs
@@ -6,13 +6,12 @@ public class floor : MonoBehaviour
 {
     [SerializeField] private GameObject floorPlane;
     private MeshRenderer rend;
-    // Start is called before the first frame update
+    
     void Start()
     {
         rend = floorPlane.GetComponent<MeshRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         rend.material.mainTextureOffset = new Vector2(0f, -Time.time * 3f);
